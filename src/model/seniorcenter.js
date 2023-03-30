e10b3f60a4490b1034b356c50dfbc2e2ffe89013
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2023-03-30 16:16:20 +0900

    Docs: Seniorcenter 스키마 작성

---
+++ src/model/seniorcenter.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+const {Schema} = mongoose;
+
+const seniorcenterSchema = new Schema({
+
+// _id
+// SIGUN_NM
+// BIZPLC_NM
+// LICENSG_DE
+// BSN_STATE_NM
+// ENTRNC_FNOP
+// REFINE_ROADNM_ADDR
+// REFINE_LOTNO_ADDR
+// REFINE_ZIPNO
+// REFINE_WGS84_LAT
+// REFINE_WGS84_LOGT
+    _id :{
+        type : String
+    },
+    SIGUN_NM :{ //소재지 도시
+        type : String
+    },
+    BIZPLC_NM :{    //경로당 이름
+        type : String
+    },
+    LICENSG_DE : {  //인허가 날짜
+        type: String
+    },
+    BSN_STATE_NM : {    //현재 운영여부
+        type: String
+    },
+    ENTRNC_FNOP : {    //입소 정원
+        type: String
+    },
+    REFINE_ROADNM_ADDR : {    //경로당 도로명 주소
+        type: String
+    },
+    REFINE_LOTNO_ADDR : {    //경로당 주소
+        type: String
+    },
+    REFINE_ZIPNO : {    //경로당 우편번호
+        type: String
+    },
+    REFINE_WGS84_LAT : {    //경로당 위도
+        type: String
+    },
+    REFINE_WGS84_LOGT : {    //경로당 경도
+        type: String
+    }
+});
+
+export default mongoose.model('Seniorcenter', seniorcenterSchema,"Seniorcenter");
